1139e5196524986776554f53edc2199341e8dfba
ramindu <rami.desilva@gmail.com>
2020-12-21 11:14:31 +0530

    Add FTP/SFTP related fixes

--- component/src/main/java/io/siddhi/extension/io/file/util/Constants.java
+++ component/src/main/java/io/siddhi/extension/io/file/util/Constants.java
@@ -50,6 +50,9 @@ public class Constants {
     public static final String HEADER_PRESENT = "header.present";
     public static final String READ_ONLY_HEADER = "read.only.header";
     public static final String CRON_EXPRESSION = "cron.expression";
+    public static final String FILE_NAME_PATTERN = "file.name.pattern";
+    public static final String FILE_NAME_PATTERN_PROPERTY_NAME = "fileNamePattern";
+    public static final String FILE_SYSTEM_OPTIONS = "file.system.options";
 
     /* configuration param values*/
     public static final String MOVE = "move";
